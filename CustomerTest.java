73a40637270fba0ab35389bf305bb658147c3d6b
harsono-sonny <sonix911@gmail.com>
2017-06-03 21:53:21 -0600

    Update CustomerTest.java

--- CustomerTest.java
+++ CustomerTest.java
@@ -1,1 +1,40 @@
-import org.junit.Test;import static org.junit.Assert.*;/** * Created by Sonny on 6/3/2017. */public class CustomerTest {    Customer sonny = new Customer("Sonny");    @Test    public void getName() throws Exception {        assertEquals("Sonny", sonny.getName());    }    @Test    public void testUnhappy() throws Exception {        assertFalse(sonny.isHappy());    }    @Test    public void testHappy() throws Exception {        sonny.eatFood();        assertTrue(sonny.isHappy());    }}
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sonny on 6/3/2017.
+ */
+
+/**
+ * This is the customer test class.
+ */
+public class CustomerTest {
+    Customer sonny = new Customer("Sonny");
+
+    /**
+     * Example of assertEquals.
+     */
+    @Test
+    public void getName() throws Exception {
+        assertEquals("Sonny", sonny.getName());
+    }
+
+    /**
+     * Example of assertFalse.
+     */
+    @Test
+    public void testUnhappy() throws Exception {
+        assertFalse(sonny.isHappy());
+    }
+
+    /**
+     * Example of assertTrue.
+     */
+    @Test
+    public void testHappy() throws Exception {
+        sonny.eatFood();
+        assertTrue(sonny.isHappy());
+    }
+
+}
