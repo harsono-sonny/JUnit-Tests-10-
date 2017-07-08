794f3e27f23939e78dd5820651c74ed33331ea8b
harsono-sonny <sonix911@gmail.com>
2017-07-08 17:32:21 -0600

    Update TimeoutTest.java

--- TimeoutTest.java
+++ TimeoutTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class TimeoutTest {
  
-  @Test (timeout = 1000) --The timeout counter is in millisecounds so you will want to adjust this number based on how much is getting done in the loop then add a bit for wiggle room
+  @Test (timeout = 1000) /**The timeout counter is in millisecounds so you will want to adjust this number based on how much is getting done in the loop then add a bit for wiggle room*/
   public void test() {
     int three = InfiniteLoop.Loop(1);
     assertEquals(1, three, 0);
