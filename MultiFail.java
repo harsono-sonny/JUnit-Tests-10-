d43cb5517ce962e7f14f1a6a1abe57ae44d3cb38
harsono-sonny <sonix911@gmail.com>
2017-07-08 17:49:07 -0600

    Create MultiFail.java

---
+++ MultiFail.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+* Multi Test Execution
+*/
+
+/**
+* A class that will run a multi test on AssertNull, and InfiniteLoop with a timeout counter
+*/
+
+public class MultiFail {
+  
+  @Test (timeout = 500)
+  public void test1() {
+    int three = InfiniteLoop.Loop(1);
+    assertEquals(1, three, 0);
+  }
+  @Test
+    public void test2() {
+    int three = Math.add(4,5);
+    assertEquals(9, three, 0);
+  }
+    @Test
+    public void test3() {
+    int four = Math.add(4,4);
+    assertEquals(9, four, 0);
+  }
+    @Test
+    public void test4() {
+      ArrayList b = NullPoint.initializer(0);
+      assertNull(b);
+  }
+    @Test (timeout = 500)
+    public void test1() {
+      int three = InfiniteLoop.Loop(1);
+      assertEquals(1, three, 0);
+  }
+}
